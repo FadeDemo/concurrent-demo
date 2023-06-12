813cf7b10fbc564b4795798c2a7ec1a315cdb4a2
paigeman <53284808+paigeman@users.noreply.github.com>
2023-06-12 13:25:00 +0800

    update CompletableFutureExample.java

--- basic/src/main/java/org/fade/demo/concurrent/future/CompletableFutureExample.java
+++ basic/src/main/java/org/fade/demo/concurrent/future/CompletableFutureExample.java
@@ -1,6 +1,7 @@
 package org.fade.demo.concurrent.future;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * CompletableFuture示例
@@ -26,6 +27,53 @@ public class CompletableFutureExample {
         // 等待所有任务完成
 //        CompletableFuture.allOf(task3).join();
         task3.join();
+        // 消费之后不能再消费了
+//        CompletableFuture<String> task4 = CompletableFuture.completedFuture("Hello World!")
+//                .whenComplete((res, ex) -> {
+//                    System.out.println(res);
+//                    ex.printStackTrace();
+//                });
+        CompletableFuture<String> task4 = CompletableFuture.completedFuture("Hello World!");
+        // 处理异常
+        CompletableFuture<String> task5 = CompletableFuture.supplyAsync(() -> {
+            if (true) {
+                throw new RuntimeException();
+            }
+            return "hello";
+        }).handle((res, ex) -> res == null ? "error" : res);
+        CompletableFuture<String> task6
+                = CompletableFuture.supplyAsync(() -> {
+            if (true) {
+                throw new RuntimeException("Computation error!");
+            }
+            return "hello!";
+        }).exceptionally(ex -> {
+            System.out.println(ex.toString());// CompletionException
+            return "world!";
+        });
+        // 本身是异常
+        CompletableFuture<String> task7 = new CompletableFuture<>();
+        task7.completeExceptionally(new RuntimeException("Task 7: exception"));
+        // thenCompose
+        CompletableFuture<String> task8
+                = CompletableFuture.supplyAsync(() -> "hello!")
+                .thenCompose(s -> CompletableFuture.supplyAsync(() -> s + "world!"));
+        // thenCombine
+        CompletableFuture<String> task9
+                = CompletableFuture.supplyAsync(() -> "hello!")
+                .thenCombine(CompletableFuture.supplyAsync(
+                        () -> "world!"), (s1, s2) -> s1 + s2)
+                .thenCompose(s -> CompletableFuture.supplyAsync(() -> s + "nice!"));
+        try {
+            System.out.println("Task 4: " + task4.get());
+            System.out.println("Task 5: " + task5.get());
+            System.out.println("Task 6: " + task6.get());
+            System.out.println("Task 8: " + task8.get());
+            System.out.println("Task 9: " + task9.get());
+            task7.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         System.out.println("All tasks completed");
     }
 
